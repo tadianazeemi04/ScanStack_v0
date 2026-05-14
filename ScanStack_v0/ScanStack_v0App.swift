7d7c775a0c9e09ede2983d08846de6c7ded02d68
Tadian Ahmad Azeemi <tadianiosdev@gmail.com>
2026-05-14 09:43:51 +0500

    Initial Commit

---
+++ ScanStack_v0/ScanStack_v0App.swift
@@ -0,0 +1,21 @@
+//
+//  ScanStack_v0App.swift
+//  ScanStack_v0
+//
+//  Created by Tadian Ahmad Azeemi on 14/05/2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct ScanStack_v0App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
